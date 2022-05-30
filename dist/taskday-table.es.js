2224e45bf3d8a5bae381b9d5e389cc34dcd0314e
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-05-30 09:47:35 +0200

    Update

--- dist/taskday-table.es.js
+++ dist/taskday-table.es.js
@@ -22,18 +22,19 @@ const _openBlock = window["Vue"].openBlock;
 const _createElementBlock = window["Vue"].createElementBlock;
 const _toDisplayString = window["Vue"].toDisplayString;
 const _unref = window["Vue"].unref;
-const _createVNode = window["Vue"].createVNode;
-const _withModifiers = window["Vue"].withModifiers;
 const _createTextVNode = window["Vue"].createTextVNode;
 const _resolveComponent = window["Vue"].resolveComponent;
 const _withCtx = window["Vue"].withCtx;
+const _createVNode = window["Vue"].createVNode;
 const _createBlock = window["Vue"].createBlock;
+const _withModifiers = window["Vue"].withModifiers;
 const _hoisted_1 = { class: "hidden md:block w-full" };
 const _hoisted_2 = { class: "w-full" };
-const _hoisted_3 = /* @__PURE__ */ _createElementVNode("th", { class: "px-2 py-1 text-left" }, "Title", -1);
-const _hoisted_4 = ["onClick"];
-const _hoisted_5 = { class: "flex items-center cursor-pointer" };
-const _hoisted_6 = {
+const _hoisted_3 = { class: "border-t" };
+const _hoisted_4 = /* @__PURE__ */ _createElementVNode("th", { class: "px-4 py-2 text-left" }, "Title", -1);
+const _hoisted_5 = ["onClick"];
+const _hoisted_6 = { class: "flex items-center justify-between cursor-pointer" };
+const _hoisted_7 = {
   key: 0,
   xmlns: "http://www.w3.org/2000/svg",
   class: "h-5 w-5 ml-2",
@@ -42,15 +43,15 @@ const _hoisted_6 = {
   stroke: "currentColor",
   "stroke-width": "2"
 };
-const _hoisted_7 = /* @__PURE__ */ _createElementVNode("path", {
+const _hoisted_8 = /* @__PURE__ */ _createElementVNode("path", {
   "stroke-linecap": "round",
   "stroke-linejoin": "round",
   d: "M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
 }, null, -1);
-const _hoisted_8 = [
-  _hoisted_7
+const _hoisted_9 = [
+  _hoisted_8
 ];
-const _hoisted_9 = {
+const _hoisted_10 = {
   key: 1,
   xmlns: "http://www.w3.org/2000/svg",
   class: "h-5 w-5 ml-2",
@@ -59,15 +60,15 @@ const _hoisted_9 = {
   stroke: "currentColor",
   "stroke-width": "2"
 };
-const _hoisted_10 = /* @__PURE__ */ _createElementVNode("path", {
+const _hoisted_11 = /* @__PURE__ */ _createElementVNode("path", {
   "stroke-linecap": "round",
   "stroke-linejoin": "round",
   d: "M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
 }, null, -1);
-const _hoisted_11 = [
-  _hoisted_10
+const _hoisted_12 = [
+  _hoisted_11
 ];
-const _hoisted_12 = {
+const _hoisted_13 = {
   key: 2,
   xmlns: "http://www.w3.org/2000/svg",
   class: "h-5 w-5 ml-2",
@@ -76,32 +77,53 @@ const _hoisted_12 = {
   stroke: "currentColor",
   "stroke-width": "2"
 };
-const _hoisted_13 = /* @__PURE__ */ _createElementVNode("path", {
+const _hoisted_14 = /* @__PURE__ */ _createElementVNode("path", {
   "stroke-linecap": "round",
   "stroke-linejoin": "round",
   d: "M8 9l4-4 4 4m0 6l-4 4-4-4"
 }, null, -1);
-const _hoisted_14 = [
-  _hoisted_13
+const _hoisted_15 = [
+  _hoisted_14
 ];
-const _hoisted_15 = { class: "divide-y" };
-const _hoisted_16 = {
-  colspan: "99",
-  class: "py-2"
+const _hoisted_16 = { class: "" };
+const _hoisted_17 = { class: "px-4 py-2" };
+const _hoisted_18 = /* @__PURE__ */ _createElementVNode("span", null, null, -1);
+const _hoisted_19 = { class: "px-4 py-2" };
+const _hoisted_20 = { class: "flex justify-start items-center space-x-2 w-full" };
+const _hoisted_21 = { class: "flex justify-end items-center space-x-2" };
+const _hoisted_22 = /* @__PURE__ */ _createElementVNode("svg", {
+  xmlns: "http://www.w3.org/2000/svg",
+  class: "h-6 w-6",
+  fill: "none",
+  viewBox: "0 0 24 24",
+  stroke: "currentColor",
+  "stroke-width": "2"
+}, [
+  /* @__PURE__ */ _createElementVNode("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    d: "M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+  })
+], -1);
+const _hoisted_23 = /* @__PURE__ */ _createElementVNode("p", null, "Delete", -1);
+const _hoisted_24 = { class: "border-t border-b" };
+const _hoisted_25 = {
+  class: "py-2",
+  colspan: "99"
 };
-const _hoisted_17 = ["onSubmit"];
-const _hoisted_18 = { class: "px-2 py-2" };
-const _hoisted_19 = /* @__PURE__ */ _createElementVNode("span", null, null, -1);
-const _hoisted_20 = { class: "px-2 py-2" };
-const _hoisted_21 = { class: "flex justify-start items-center space-x-2 w-full" };
-const _hoisted_22 = { class: "flex flex-col gap-2 md:hidden" };
-const _hoisted_23 = { class: "flex items-center gap-3 mt-2" };
+const _hoisted_26 = ["onSubmit"];
+const _hoisted_27 = { class: "flex flex-col gap-2 md:hidden" };
+const _hoisted_28 = ["onSubmit"];
+const _hoisted_29 = { class: "flex items-center gap-3 mt-2" };
 const VFormInput = window["Components"].VFormInput;
 const VFieldWrapper = window["Components"].VFieldWrapper;
 const VCard = window["Components"].VCard;
-const VContainer = window["Components"].VContainer;
 const useCardForm = window["Components"].useCardForm;
 const useSorter = window["Components"].useSorter;
+const VDropdown = window["Components"].VDropdown;
+const VDropdownButton = window["Components"].VDropdownButton;
+const VDropdownItems = window["Components"].VDropdownItems;
+const VDropdownItem = window["Components"].VDropdownItem;
 const _sfc_main = /* @__PURE__ */ _defineComponent({
   props: {
     project: {
@@ -111,117 +133,160 @@ const _sfc_main = /* @__PURE__ */ _defineComponent({
   },
   setup(__props) {
     const props = __props;
-    const { form, store } = useCardForm();
+    const { form, store, destroy } = useCardForm();
     const { sortBy, isDesc, isCurrent } = useSorter();
     function submit() {
-      store(props.project);
+      if (!form.processing) {
+        store(props.project);
+      }
+    }
+    function remove(card) {
+      confirm(`Are you sure to delete "${card.title}"?`) && destroy(card);
     }
     return (_ctx, _cache) => {
       const _component_Link = _resolveComponent("Link");
-      return _openBlock(), _createBlock(_unref(VContainer), null, {
-        default: _withCtx(() => [
-          _createElementVNode("div", _hoisted_1, [
-            _createVNode(_unref(VCard), null, {
-              default: _withCtx(() => [
-                _createElementVNode("table", _hoisted_2, [
-                  _createElementVNode("thead", null, [
-                    _createElementVNode("tr", null, [
-                      _hoisted_3,
-                      (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.fields, (field) => {
-                        return _openBlock(), _createElementBlock("th", {
-                          class: "px-2 py-1 text-left",
-                          onClick: ($event) => _unref(sortBy)(field)
-                        }, [
-                          _createElementVNode("div", _hoisted_5, [
-                            _createElementVNode("span", null, _toDisplayString(field.title), 1),
-                            _unref(isCurrent)(field) && !_unref(isDesc)(field) ? (_openBlock(), _createElementBlock("svg", _hoisted_6, _hoisted_8)) : _unref(isCurrent)(field) && _unref(isDesc)(field) ? (_openBlock(), _createElementBlock("svg", _hoisted_9, _hoisted_11)) : (_openBlock(), _createElementBlock("svg", _hoisted_12, _hoisted_14))
-                          ])
-                        ], 8, _hoisted_4);
-                      }), 256))
-                    ])
-                  ]),
-                  _createElementVNode("tbody", _hoisted_15, [
-                    _createElementVNode("tr", null, [
-                      _createElementVNode("td", _hoisted_16, [
-                        _createElementVNode("form", {
-                          onSubmit: _withModifiers(submit, ["prevent"])
-                        }, [
-                          _createVNode(_unref(VFormInput), {
-                            placeholder: "Create a card...",
-                            modelValue: _unref(form).title,
-                            "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _unref(form).title = $event)
-                          }, null, 8, ["modelValue"])
-                        ], 40, _hoisted_17)
-                      ])
-                    ]),
-                    (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.cards, (card) => {
-                      return _openBlock(), _createElementBlock("tr", {
-                        key: __props.project.id + "-" + card.id
+      return _openBlock(), _createElementBlock(_Fragment, null, [
+        _createElementVNode("div", _hoisted_1, [
+          _createVNode(_unref(VCard), { style: { "padding": "0" } }, {
+            default: _withCtx(() => [
+              _createElementVNode("table", _hoisted_2, [
+                _createElementVNode("thead", null, [
+                  _createElementVNode("tr", _hoisted_3, [
+                    _hoisted_4,
+                    (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.fields, (field) => {
+                      return _openBlock(), _createElementBlock("th", {
+                        class: "px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-800 border-l",
+                        onClick: ($event) => _unref(sortBy)(field)
                       }, [
-                        _createElementVNode("td", _hoisted_18, [
-                          _createVNode(_component_Link, {
-                            class: "text-base hover:underline",
-                            href: _ctx.route("cards.show", card)
-                          }, {
-                            default: _withCtx(() => [
-                              _createTextVNode(_toDisplayString(card.title), 1)
-                            ]),
-                            _: 2
-                          }, 1032, ["href"]),
-                          _hoisted_19
-                        ]),
-                        (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.fields, (field) => {
-                          return _openBlock(), _createElementBlock("td", _hoisted_20, [
-                            _createElementVNode("div", _hoisted_21, [
-                              (_openBlock(), _createBlock(_unref(VFieldWrapper), {
-                                key: field.id,
-                                card,
-                                field
-                              }, null, 8, ["card", "field"]))
-                            ])
-                          ]);
-                        }), 256))
-                      ]);
-                    }), 128))
-                  ])
-                ])
-              ]),
-              _: 1
-            })
-          ]),
-          _createElementVNode("div", _hoisted_22, [
-            (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.cards, (card) => {
-              return _openBlock(), _createBlock(_unref(VCard), null, {
-                default: _withCtx(() => [
-                  _createVNode(_component_Link, {
-                    class: "text-base hover:underline",
-                    href: _ctx.route("cards.show", card)
-                  }, {
-                    default: _withCtx(() => [
-                      _createTextVNode(_toDisplayString(card.title), 1)
-                    ]),
-                    _: 2
-                  }, 1032, ["href"]),
-                  _createElementVNode("div", _hoisted_23, [
-                    (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(card.project.fields, (field) => {
-                      return _openBlock(), _createElementBlock("div", {
-                        key: card.id + "" + field.handle
-                      }, [
-                        _createVNode(_unref(VFieldWrapper), {
-                          field: __spreadValues(__spreadValues({}, field), card.customFields[field.handle]),
-                          card
-                        }, null, 8, ["field", "card"])
-                      ]);
-                    }), 128))
+                        _createElementVNode("div", _hoisted_6, [
+                          _createElementVNode("span", null, _toDisplayString(field.title), 1),
+                          _unref(isCurrent)(field) && !_unref(isDesc)(field) ? (_openBlock(), _createElementBlock("svg", _hoisted_7, _hoisted_9)) : _unref(isCurrent)(field) && _unref(isDesc)(field) ? (_openBlock(), _createElementBlock("svg", _hoisted_10, _hoisted_12)) : (_openBlock(), _createElementBlock("svg", _hoisted_13, _hoisted_15))
+                        ])
+                      ], 8, _hoisted_5);
+                    }), 256))
                   ])
                 ]),
-                _: 2
-              }, 1024);
-            }), 256))
-          ])
+                _createElementVNode("tbody", _hoisted_16, [
+                  (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.cards, (card) => {
+                    return _openBlock(), _createElementBlock("tr", {
+                      key: __props.project.id + "-" + card.id,
+                      class: "border-t last:border-b"
+                    }, [
+                      _createElementVNode("td", _hoisted_17, [
+                        _createVNode(_component_Link, {
+                          class: "text-base hover:underline",
+                          href: _ctx.route("cards.show", card)
+                        }, {
+                          default: _withCtx(() => [
+                            _createTextVNode(_toDisplayString(card.title), 1)
+                          ]),
+                          _: 2
+                        }, 1032, ["href"]),
+                        _hoisted_18
+                      ]),
+                      (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.fields, (field) => {
+                        return _openBlock(), _createElementBlock("td", _hoisted_19, [
+                          _createElementVNode("div", _hoisted_20, [
+                            (_openBlock(), _createBlock(_unref(VFieldWrapper), {
+                              key: field.id,
+                              card,
+                              field
+                            }, null, 8, ["card", "field"]))
+                          ])
+                        ]);
+                      }), 256)),
+                      _createElementVNode("td", null, [
+                        _createElementVNode("div", _hoisted_21, [
+                          _createVNode(_unref(VDropdown), null, {
+                            default: _withCtx(() => [
+                              _createVNode(_unref(VDropdownButton), null, {
+                                default: _withCtx(() => [
+                                  _hoisted_22
+                                ]),
+                                _: 1
+                              }),
+                              _createVNode(_unref(VDropdownItems), null, {
+                                default: _withCtx(() => [
+                                  _createVNode(_unref(VDropdownItem), {
+                                    onClick: ($event) => remove(card),
+                                    class: "hover:text-red-600"
+                                  }, {
+                                    default: _withCtx(() => [
+                                      _hoisted_23
+                                    ]),
+                                    _: 2
+                                  }, 1032, ["onClick"])
+                                ]),
+                                _: 2
+                              }, 1024)
+                            ]),
+                            _: 2
+                          }, 1024)
+                        ])
+                      ])
+                    ]);
+                  }), 128)),
+                  _createElementVNode("tr", _hoisted_24, [
+                    _createElementVNode("td", _hoisted_25, [
+                      _createElementVNode("form", {
+                        onSubmit: _withModifiers(submit, ["prevent"])
+                      }, [
+                        _createVNode(_unref(VFormInput), {
+                          placeholder: "Create a card...",
+                          modelValue: _unref(form).title,
+                          "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => _unref(form).title = $event),
+                          class: "w-full border-none focus:ring-0 shadow-none border-b"
+                        }, null, 8, ["modelValue"])
+                      ], 40, _hoisted_26)
+                    ])
+                  ])
+                ])
+              ])
+            ]),
+            _: 1
+          })
         ]),
-        _: 1
-      });
+        _createElementVNode("div", _hoisted_27, [
+          _createElementVNode("form", {
+            onSubmit: _withModifiers(submit, ["prevent"])
+          }, [
+            _createVNode(_unref(VFormInput), {
+              placeholder: "Create a card...",
+              modelValue: _unref(form).title,
+              "onUpdate:modelValue": _cache[1] || (_cache[1] = ($event) => _unref(form).title = $event),
+              class: "w-full border-none focus:ring-0 shadow-none border-b"
+            }, null, 8, ["modelValue"])
+          ], 40, _hoisted_28),
+          (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(__props.project.cards, (card) => {
+            return _openBlock(), _createBlock(_unref(VCard), null, {
+              default: _withCtx(() => [
+                _createVNode(_component_Link, {
+                  class: "text-base hover:underline",
+                  href: _ctx.route("cards.show", card)
+                }, {
+                  default: _withCtx(() => [
+                    _createTextVNode(_toDisplayString(card.title), 1)
+                  ]),
+                  _: 2
+                }, 1032, ["href"]),
+                _createElementVNode("div", _hoisted_29, [
+                  (_openBlock(true), _createElementBlock(_Fragment, null, _renderList(card.project.fields, (field) => {
+                    return _openBlock(), _createElementBlock("div", {
+                      key: card.id + "" + field.handle
+                    }, [
+                      _createVNode(_unref(VFieldWrapper), {
+                        field: __spreadValues(__spreadValues({}, field), card.customFields[field.handle]),
+                        card
+                      }, null, 8, ["field", "card"])
+                    ]);
+                  }), 128))
+                ])
+              ]),
+              _: 2
+            }, 1024);
+          }), 256))
+        ])
+      ], 64);
     };
   }
 });
--- resources/js/components/Component.vue
+++ resources/js/components/Component.vue
@@ -1,83 +1,105 @@
 <template>
-  <VContainer>
-    <div class="hidden md:block w-full">
-      <VCard>
-        <table class="w-full">
-          <thead>
-            <tr>
-              <th class="px-2 py-1 text-left">Title</th>
-              <th class="px-2 py-1 text-left" v-for="field in project.fields" @click="sortBy(field)">
-                <div class="flex items-center cursor-pointer">
-                  <span>{{ field.title }}</span>
-                  <svg v-if="isCurrent(field) && !isDesc(field)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12" />
-                  </svg>
-                  <svg v-else-if="isCurrent(field) && isDesc(field)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
-                  </svg>
-                  <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
-                  </svg>
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody class="divide-y">
-            <tr>
-              <td colspan="99" class="py-2">
-                <form @submit.prevent="submit">
-                  <VFormInput
-                    placeholder="Create a card..."
-                    v-model="form.title"
-                  ></VFormInput>
-                </form>
-              </td>
-            </tr>
-            <tr v-for="card in project.cards" :key="project.id + '-' + card.id">
-              <td class="px-2 py-2">
-                <Link
-                  class="text-base hover:underline"
-                  :href="route('cards.show', card)"
-                >
-                  {{ card.title }}
-                </Link>
-                <span></span>
-              </td>
-              <td class="px-2 py-2" v-for="field in project.fields">
-                <div class="flex justify-start items-center space-x-2 w-full">
-                  <VFieldWrapper
-                    :key="field.id"
-                    :card="card"
-                    :field="field"
-                  ></VFieldWrapper>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </VCard>
-    </div>
-    <div class="flex flex-col gap-2 md:hidden">
-      <VCard v-for="card in project.cards">
-        <Link
-          class="text-base hover:underline"
-          :href="route('cards.show', card)"
-          >{{ card.title }}</Link
+  <div class="hidden md:block w-full">
+    <VCard style="padding: 0">
+      <table class="w-full">
+        <thead>
+          <tr class="border-t">
+            <th class="px-4 py-2 text-left">Title</th>
+            <th class="px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-800 border-l" v-for="field in project.fields" @click="sortBy(field)">
+              <div class="flex items-center justify-between cursor-pointer">
+                <span>{{ field.title }}</span>
+                <svg v-if="isCurrent(field) && !isDesc(field)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12" />
+                </svg>
+                <svg v-else-if="isCurrent(field) && isDesc(field)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
+                </svg>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="">
+          <tr v-for="card in project.cards" :key="project.id + '-' + card.id"  class="border-t last:border-b">
+            <td class="px-4 py-2">
+              <Link
+                class="text-base hover:underline"
+                :href="route('cards.show', card)"
+              >
+                {{ card.title }}
+              </Link>
+              <span></span>
+            </td>
+            <td class="px-4 py-2" v-for="field in project.fields">
+              <div class="flex justify-start items-center space-x-2 w-full">
+                <VFieldWrapper
+                  :key="field.id"
+                  :card="card"
+                  :field="field"
+                ></VFieldWrapper>
+              </div>
+            </td>
+            <td>
+              <div class="flex justify-end items-center space-x-2">
+                <VDropdown>
+                  <VDropdownButton>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                    </svg>
+                  </VDropdownButton>
+                  <VDropdownItems>
+                    <VDropdownItem @click="remove(card)" class="hover:text-red-600">
+                      <p>Delete</p>
+                    </VDropdownItem>
+                  </VDropdownItems>
+                </VDropdown>
+              </div>
+            </td>
+          </tr>
+          <tr class="border-t border-b">
+            <td class="py-2" colspan="99">
+              <form @submit.prevent="submit">
+                <VFormInput
+                  placeholder="Create a card..."
+                  v-model="form.title"
+                  class="w-full border-none focus:ring-0 shadow-none border-b" 
+                ></VFormInput>
+              </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </VCard>
+  </div>
+  <div class="flex flex-col gap-2 md:hidden">
+    <form @submit.prevent="submit">
+      <VFormInput
+        placeholder="Create a card..."
+        v-model="form.title"
+        class="w-full border-none focus:ring-0 shadow-none border-b" 
+      ></VFormInput>
+    </form>
+    <VCard v-for="card in project.cards">
+      <Link
+        class="text-base hover:underline"
+        :href="route('cards.show', card)"
+        >{{ card.title }}</Link
+      >
+      <div class="flex items-center gap-3 mt-2">
+        <div
+          v-for="field in card.project.fields"
+          :key="card.id + '' + field.handle"
         >
-        <div class="flex items-center gap-3 mt-2">
-          <div
-            v-for="field in card.project.fields"
-            :key="card.id + '' + field.handle"
-          >
-            <VFieldWrapper
-              :field="{ ...field, ...card.customFields[field.handle] }"
-              :card="card"
-            />
-          </div>
+          <VFieldWrapper
+            :field="{ ...field, ...card.customFields[field.handle] }"
+            :card="card"
+          />
         </div>
-      </VCard>
-    </div>
-  </VContainer>
+      </div>
+    </VCard>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -88,6 +110,11 @@ import {
   VContainer,
   useCardForm,
   useSorter,
+  VDropdown,
+  VDropdownButton,
+  VDropdownItems,
+  VDropdownItem,
+  VButton,
 } from "taskday";
 
 const props = defineProps({
@@ -97,10 +124,16 @@ const props = defineProps({
   },
 });
 
-const { form, store } = useCardForm();
+const { form, store, destroy } = useCardForm();
 const { sortBy, isDesc, isCurrent } = useSorter();
 
 function submit() {
-  store(props.project);
+  if (!form.processing) {
+    store(props.project);
+  }
+}
+
+function remove(card: Card) {
+  confirm(`Are you sure to delete "${card.title}"?`) && destroy(card);
 }
 </script>
